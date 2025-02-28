97c05a3e7bd89598458550ff753746bff77be4bf
Fabien Fournier <140694478+uncodestudio@users.noreply.github.com>
2025-02-28 11:16:07 +0400

    Updating rotation-badge

---
+++ rotation-badge.js
@@ -0,0 +1,6 @@
+gsap.to(".home-header_badge-wrapper", {
+  rotation: 360, // Rotation complète
+  duration: 20, // Durée de 3 secondes
+  repeat: -1, // Répétition infinie
+  ease: "linear" // Mouvement fluide sans accélération
+});
